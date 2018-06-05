e1b0e538e7cf668aec8c2511d3dc961508976019
hmc123321 <37052560+hmc123321@users.noreply.github.com>
2018-06-05 17:16:11 +0800

    上传stm程序
    实验upload，stm32blink文件

---
+++ stm_blink.ino
@@ -0,0 +1,15 @@
+void setup() {
+ pinMode (PA5,OUTPUT); // put your setup code here, to run once:
+pinMode (PA6,OUTPUT);
+}
+
+void loop() {
+ digitalWrite (PA6,LOW);
+ digitalWrite (PA5,HIGH);
+ delay (1000);
+ digitalWrite (PA5,LOW);
+  delay (1000);
+ 
+ 
+
+}
