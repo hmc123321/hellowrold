fbe3197476bbea4e9abbb37bd5c3d8d85b6bad2a
hmc123321 <37052560+hmc123321@users.noreply.github.com>
2018-06-05 16:53:21 +0800

    添加了注释

--- arduino_Blink.ino
+++ arduino_Blink.ino
@@ -1,9 +1,9 @@
 void setup(){
-  pinMode(13,OUTPUT);
+  pinMode(13,OUTPUT);//定义引脚13输出
 }
 void loop(){
-  digitalWrite(13,HIGH);
-  delay(1000);
+  digitalWrite(13,HIGH);//给13引脚高电平
+  delay(1000);//延时1秒
   digitalWrite(13,LOW);
   delay(1000)l;
 }
